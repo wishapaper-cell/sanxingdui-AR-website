ff3d3d08f4befef61acb2433869386d5f5f6d0ec
Tomato <wishapaper@gmail.com>
2026-07-11 00:06:39 +0800

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GestureMaskSwitch from './GestureMaskSwitch'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <GestureMaskSwitch />
+  </React.StrictMode>,
+)
